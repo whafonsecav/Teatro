f9ba2edd887e003bad33f238ed193563467b996e
whafonsecav <whafonsecav@poligran.edu.co>
2026-04-17 23:07:02 -0500

    Fix detached event handlers on Desktop

--- app.js
+++ app.js
@@ -120,6 +120,7 @@ function detectAndInit() {
    VISTA ESCRITORIO
    ═══════════════════════════════════════════════════════════ */
 function initDesktop() {
+  setupDesktopEvents();
   generatePin();
 }
 
