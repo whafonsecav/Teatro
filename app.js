9527edad0e9ebf2e78a89abf494b5fdd69795d6c
whafonsecav <whafonsecav@poligran.edu.co>
2026-04-17 19:29:09 -0500

    Fix UDP block on 4G via TCP TURN

--- app.js
+++ app.js
@@ -164,6 +164,26 @@ function initDesktopPeer(peerId) {
       host: '0.peerjs.com',
       port: 443,
       secure: true,
+      config: {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ]
+      },
       debug: 1,
     });
 
@@ -1156,6 +1176,26 @@ function mobileConnect() {
       host: '0.peerjs.com',
       port: 443,
       secure: true,
+      config: {
+        iceServers: [
+          { urls: 'stun:stun.l.google.com:19302' },
+          {
+            urls: 'turn:openrelay.metered.ca:80',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          },
+          {
+            urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+          }
+        ]
+      },
       debug: 1,
     });
 
